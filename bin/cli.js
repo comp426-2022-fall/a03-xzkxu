c367240048a41c20b782a97e3f4f383de28ef714
Zhaokun Xu <zhaokunxu@ZhaokundeMBP-162.lan>
2022-11-22 17:13:04 -0500

    test

---
+++ bin/cli.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+
+import minimist from 'minimist'
+import {roll} from "../lib/roll.js"
+
+const args = minimist(process.argv.slice(2))
+
+const sides = args.sides || 6;
+const dice = args.dice || 2;
+const rolls = args.rolls || 1;
+
+console.log(JSON.stringify(roll(sides, dice, rolls)));
+
+process.exit(0);
